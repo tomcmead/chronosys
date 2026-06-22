98666ebef674b13ff42bd3c115854b4f9fb7415d
tomcmead <tomcmead0@gmail.com>
2026-06-22 18:39:28 +1000

    Add logger with log level set by RUST_LOG env variable

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,9 @@
-fn main() {}
+fn main() {
+    // Setup env_logger to read from the RUST_LOG env variable
+    #[cfg(feature = "logging")]
+    {
+        let env = env_logger::Env::default().default_filter_or("info");
+        env_logger::Builder::from_env(env).init();
+        log::debug!("Log Level: {:?}", log::max_level());
+    }
+}
